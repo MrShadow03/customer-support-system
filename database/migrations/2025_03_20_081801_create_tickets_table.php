dad8af1abb372cbdd9afe54e2eead57fccc2e9ea
Galib <gj.emon35@gmail.com>
2025-03-20 14:35:01 +0600

    initial setup

---
+++ database/migrations/2025_03_20_081801_create_tickets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('subject');
+            $table->text('description');
+            $table->enum('category', ['Technical', 'Billing', 'General']);
+            $table->enum('priority', ['Low', 'Medium', 'High']);
+            $table->string('attachment')->nullable();
+            $table->enum('status', ['Open', 'In Progress', 'Resolved', 'Closed'])->default('Open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
